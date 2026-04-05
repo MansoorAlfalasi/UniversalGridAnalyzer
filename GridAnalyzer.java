0f19853b63bfa09519d7d76f3e73dc54f7c58116
MansoorAlfalasi <mansoormubarak50@gmail.com>
2026-04-06 00:57:45 +0400

    Update GridAnalyzer.java
    1

--- GridAnalyzer.java
+++ GridAnalyzer.java
@@ -1,1 +1,35 @@
+public class GridAnalyzer{
+ public static void main (String[] args){
+    int[][] grid= new int[6][6]; 
 
+ }
+ public static int[] rowSum(int[][] grid){
+    int[] rowSum = new int[grid.length];
+
+    for( int row = 0; row<grid.length; row++){
+        for(int col = 0; col<grid[0].length; col++){
+        rowSum[row] += grid[row][col];
+        }
+
+
+    }
+
+    return rowSum;
+ }
+  public static int[] colSum(int[][] grid){
+    int[] rowSum = new int[grid[0].length];
+    for(int col = 0; col<grid[0].length; col++){
+    for( int row = 0; row<grid.length; row++){
+        rowSum[col] += grid[row][col];
+        }
+    }
+    return rowSum;
+ }
+
+
+
+
+
+
+}
+  
