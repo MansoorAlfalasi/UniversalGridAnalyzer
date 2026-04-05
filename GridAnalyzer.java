fb632eda97f08378879946ed68debc8ef13f1e8e
MansoorAlfalasi <mansoormubarak50@gmail.com>
2026-04-06 01:45:14 +0400

    Update GridAnalyzer.java

--- GridAnalyzer.java
+++ GridAnalyzer.java
@@ -25,6 +25,48 @@ public class GridAnalyzer{
     }
     return rowSum;
  }
+  // day 2 night
+
+
+ public int[] max(int[][] grid){
+    int max=grid[0][0];
+    int r=0;
+    int c=0;
+    int[] MRC= {max, r, c};
+
+    for (int row = 0; row < grid.length; row++){
+        for (int col = 0; col < grid[0].length; col++){
+            if (grid[row][col] > max){
+                max = grid[row][col];
+                r=row;
+                c=col;
+            }
+
+        }
+    }
+
+    return MRC;
+ }
+
+
+     public int[] min(int[][] grid) {
+    int max=grid[0][0];
+    int r=0;
+    int c=0;
+    int[] MRC= {max, r, c};
+
+    for (int row = 0; row < grid.length; row++){
+        for (int col = 0; col < grid[0].length; col++){
+            if (grid[row][col] < max){
+                max = grid[row][col];
+                r=row;
+                c=col;
+            }
+        }
+     }
+     return MRC;
+    }
+
 
 
 
